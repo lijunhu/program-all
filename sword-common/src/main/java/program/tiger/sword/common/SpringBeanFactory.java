df7cac9553d26b31356ebec7cf849a7240ff82fc
李俊虎 <junhu.li@qunar.com>
2018-11-05 14:45:12 +0800

    初始化工程

---
+++ sword-common/src/main/java/program/tiger/sword/common/SpringBeanFactory.java
@@ -0,0 +1,62 @@
+package program.tiger.sword.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring beanFactory工具类
+ * @author TigerLee
+ * @date 2018年11月5日14:42:32
+ */
+
+@Component
+public class SpringBeanFactory implements ApplicationContextAware {
+
+    /**
+     * 上下文对象实例
+     */
+    private ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext){
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取applicationContext
+     * @return
+     */
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 通过name获取 Bean.
+     * @param name
+     * @return
+     */
+    public Object getBean(String name){
+        return getApplicationContext().getBean(name);
+    }
+
+    /**
+     * 通过class获取Bean.
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public <T> T getBean(Class<T> clazz){
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public <T> T getBean(String name,Class<T> clazz){
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
